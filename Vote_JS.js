c8c5ccb8eb7b058ea394af208326fa69428d0dbd
Elliot T Simpson <Falcon5783@aol.com>
2015-05-28 13:20:24 -0700

    git push to create express branch

---
+++ Vote_JS.js
@@ -0,0 +1,12 @@
+CatStats.prototype.generateRandom = function() {
+  return Math.ceil(Math.random() * 100);
+}
+
+function CatStats() {
+  this.name = name
+  this.rating = CatStats.generateRandom
+}
+
+var eachCat = new CatStats(name, rating){
+
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is a vote tracker project for the CodeFellows May SEA-B30 bootcamp. This is to practice and demonstrate jQuery and JS methods.
